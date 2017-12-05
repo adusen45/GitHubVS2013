3ac0ca6f39de721f67e24fd944234ffccd3dc38d
adusen45 <adusen45@gmail.com>
2017-12-05 15:22:01 +0100

    Put Biginteger Add function

---
+++ CalculatorKernel/Number.vb
@@ -0,0 +1,8 @@
+﻿Imports System.Numerics
+
+Public Class Number
+    Public Function Add(num1 As BigInteger, num2 As BigInteger) As BigInteger
+        Return num1 + num2
+
+    End Function
+End Class
